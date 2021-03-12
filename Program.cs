829d41c1aca7b2c6312ae884620d98d06b2a21ce
liyana0144 <78543927+liyana0144@users.noreply.github.com>
2021-03-12 23:14:35 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3_hw_jaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string mode = Console.ReadLine();
+            double  xt = double .Parse(Console.ReadLine());
+            double  yp = double .Parse(Console.ReadLine());
+            if (mode =="time" || mode == "price")
+            {
+                if (xt < 0)
+                {
+                    Console.Write("invalid mouse position");
+                }
+                else if (mode == "time")
+                {
+                    yp = Math.Pow((xt - 1), 2);
+                    Console.Write("{0},{1}",xt, yp);
+                }
+                else if (mode == "price")
+                {
+                    xt = Math.Sqrt(yp) + 1;
+                    Console.Write("{0},{1}", xt, yp);
+                }
+                    
+            }
+            else
+            {
+                Console.Write("invalid mode");
+            }
+            Console.ReadLine();
+        }
+    }
+}
