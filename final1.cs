10b4c0de9f8f118dd575243397c2896fe2d12bf1
thlyn01441048 <liyana.ptpc@mail.kmutt.ac.th>
2021-06-13 17:06:12 +0700

    ข้อ1

---
+++ final1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp5_f_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+            int i, j;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("element - [{0},{1}] : ", i, j);
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("matrix");
+
+            for (i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                for (j = 0; j < 3; j++)
+                    Console.Write(arr[i, j]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
